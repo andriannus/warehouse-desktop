656a65e4cc56714bc4239cbcaf8177b92c542619
Andriannus Parasian <andriannus.parasian@amarbank.co.id>
2020-07-24 23:29:40 +0700

    install packages and use bulma

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue';
 import axios from 'axios';
 
-import App from './App';
-import router from './router';
-import store from './store';
+import App from '@/App';
+import router from '@/router';
+import store from '@/store';
+
+import '@/main.scss';
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'));
 Vue.http = Vue.prototype.$http = axios;
