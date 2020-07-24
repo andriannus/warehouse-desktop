8a64a3e66a1e5a03c56882018f7074867191b6c9
Andriannus Parasian <andriannus.parasian@amarbank.co.id>
2020-07-24 23:28:42 +0700

    create login page

---
+++ src/renderer/pages/login/Login.vue
@@ -0,0 +1,15 @@
+<template>
+  <plain-layout>
+    <div class="columns">
+      <div class="column is-two-fifths is-offset-one-third">
+        <div class="card has-text-left">
+          <div class="card-header">
+            <div class="card-header-title">Login Admin</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </plain-layout>
+</template>
+
+<script src="./login.js"></script>
---
+++ src/renderer/pages/login/login.js
@@ -0,0 +1,7 @@
+import PlainLayout from '@/shared/layouts/plain-layout/PlainLayout.vue';
+
+export default {
+  components: {
+    PlainLayout,
+  },
+};
