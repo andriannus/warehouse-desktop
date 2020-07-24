1194225581ca1c63603ffdce7e81b6b32748cf45
Andriannus Parasian <andriannus.parasian@amarbank.co.id>
2020-07-24 23:28:10 +0700

    update config project

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,30 @@
+const { EOL } = require("os");
+
 module.exports = {
   root: true,
-  parser: 'babel-eslint',
+  parser: "babel-eslint",
   parserOptions: {
-    sourceType: 'module'
+    sourceType: "module",
   },
   env: {
     browser: true,
-    node: true
+    node: true,
   },
-  extends: 'airbnb-base',
+  extends: "airbnb-base",
   globals: {
-    __static: true
+    __static: true,
   },
-  plugins: [
-    'html'
-  ],
-  'rules': {
-    'global-require': 0,
-    'import/no-unresolved': 0,
-    'no-param-reassign': 0,
-    'no-shadow': 0,
-    'import/extensions': 0,
-    'import/newline-after-import': 0,
-    'no-multi-assign': 0,
+  plugins: ["html"],
+  rules: {
+    "linebreak-style": 0,
+    "global-require": 0,
+    "import/no-unresolved": 0,
+    "no-param-reassign": 0,
+    "no-shadow": 0,
+    "import/extensions": 0,
+    "import/newline-after-import": 0,
+    "no-multi-assign": 0,
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
-  }
-}
+    "no-debugger": process.env.NODE_ENV === "production" ? 2 : 0,
+  },
+};
