79bccfd926395064cb29704a349c5c0d34ddc7b1
Andriannus Parasian <andriannus.parasian@amarbank.co.id>
2020-07-24 23:29:02 +0700

    create layouts

---
+++ src/renderer/shared/layouts/plain-layout/PlainLayout.vue
@@ -0,0 +1,9 @@
+<template>
+  <section class="hero is-fullheight">
+    <div class="hero-body">
+      <div class="container has-text-centered">
+        <slot></slot>
+      </div>
+    </div>
+  </section>
+</template>
